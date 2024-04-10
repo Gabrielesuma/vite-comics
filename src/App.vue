365046a79dbaa5d6de8eb76b190b1eb7eb319444
Gabrielesuma <gabrielesuma139@gmail.com>
2024-04-10 17:40:24 +0200

    header component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <h1 class="text-center">ciao</h1>
+  <HeaderComponent />
+  <MainComponent />
+  <FooterComponent />
 </template>
 
 <script>
+  import HeaderComponent from './components/HeaderComponent.vue';
+  import MainComponent from './components/MainComponent.vue';
+  import FooterComponent from './components/FooterComponent.vue';
   export default {
-    name: 'App'
+    name: 'App',
+    components: {
+      HeaderComponent,
+      MainComponent,
+      FooterComponent
+    }
   }
 </script>
 
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,94 @@
+<template>
+    <header class="container">
+        <img src="../assets/img/dc-logo.png" alt="Logo">
+        <ul>
+            <li v-for="(item, index) in menu" :key="index">
+                <a :href="item.url">{{ item.name }}</a>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+        data(){
+            return {
+                menu: [
+                    {
+                        name: 'CHARACTERS',
+                        url: '#'
+                    },
+                    {
+                        name: 'COMICS',
+                        url: '#'
+                    },
+                    {
+                        name: 'MOVIES',
+                        url: '#'
+                    },
+                    {
+                        name: 'TV',
+                        url: '#'
+                    },
+                    {
+                        name: 'GAMES',
+                        url: '#'
+                    },
+                    {
+                        name: 'COLLECTIBLES',
+                        url: '#'
+                    },
+                    {
+                        name: 'VIDEOS',
+                        url: '#'
+                    },
+                    {
+                        name: 'FANS',
+                        url: '#'
+                    },
+                    {
+                        name: 'NEWS',
+                        url: '#'
+                    },
+                    {
+                        name: 'SHOP',
+                        url: '#'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use '../assets/styles/partials/variables' as *;
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px 0;
+        img{
+            width: 50px;
+        }
+    }
+
+    ul{
+        list-style: none;
+        display: flex;
+
+        li{
+            padding: 0 10px;
+
+            a{
+                color: $link;
+                text-decoration: none;
+                font-size: 13px;
+                &:hover{
+                    color: $hoverlink;
+                }
+            }
+        }
+
+    }
+</style>
