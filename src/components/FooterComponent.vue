509a57ea7ff04049cd8dc17eb609ae1584817f6c
Gabrielesuma <gabrielesuma139@gmail.com>
2024-04-10 18:45:13 +0200

    first part footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,29 @@
 <template>
     <footer>
-        footer
+        <div class="first-footer">
+            <div class="container">
+                <div>
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="Logo">
+                    <span>DIGITAL COMICS</span>
+                </div>
+                <div>
+                    <img src="../assets/img/buy-comics-merchandise.png" alt="Logo">
+                    <span>DC MERCHANDISE</span>
+                </div>
+                <div>
+                    <img src="../assets/img/buy-comics-subscriptions.png" alt="Logo">
+                    <span>SUBSCRIPTION</span>
+                </div>
+                <div>
+                    <img src="../assets/img/buy-comics-shop-locator.png" alt="Logo">
+                    <span>COMIC SHOP LOCATOR</span>
+                </div>
+                <div>
+                    <img src="../assets/img/buy-dc-power-visa.svg" alt="Logo">
+                    <span>DC POWER VISA</span>
+                </div>
+            </div>
+        </div>
     </footer>
 </template>
 
@@ -10,6 +33,24 @@
     }
 </script>
 
-<style lang="scss" scoped>
 
+<style lang="scss" scoped>
+    @use '../assets/styles/partials/variables' as *;
+    div.first-footer{
+        padding: 50px 0;
+        background-color: $firstfooter;
+        font-size: 13px;
+        color: white;
+        div.container{
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            img{
+                width: 40px;
+            }
+            span{
+                padding-left: 10px;
+            }
+        }
+    }
 </style>
